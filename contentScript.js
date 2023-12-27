dbd65c5d2b16d2fe8742f2433db05b186b401395
Abdulmumin Yaqeen <avdorr12345@gmail.com>
2023-12-27 11:42:36 +0100

    customize

--- contentScript.js
+++ contentScript.js
@@ -1,36 +1,49 @@
 // This code is injected into the web page as a content script
 
 // Function to append a <style> element with custom scrollbar styles
+chrome.storage.sync.get(["SleekScrollData"], function (result) {
+  if (chrome.runtime.lastError) {
+    console.error(chrome.runtime.lastError);
+    return;
+  }
 
-let before = document.head.firstElementChild;
+  let Sdata;
+  Sdata = result.SleekScrollData;
+  console.log("Retrieved data:", Sdata);
 
-function addCustomScrollbarStyle() {
-  const scrollStyle = document.createElement("style");
-  scrollStyle.textContent = `   
-      ::-webkit-scrollbar {
-        width: 2px;
-        height:2px;
-        transition:width 0.3s ease-in-out;
-      }
-      ::-webkit-scrollbar:hover {
-        width: 4px;
-      }
-      ::-webkit-scrollbar-thumb {
-        background-color: #666;
-        border-radius: 5px;
-        max-height:30px;
-      }
-      ::-webkit-scrollbar-thumb:hover {
-        background-color: #888;
-      }
-      ::-webkit-scrollbar-track {
-        background-color:inherit;
-      }
-    
+  const width = Sdata["width"] || "10px";
+  const height = Sdata["height"] || "10px";
+  const borderRd = Sdata["borderRd"] || "0px";
+  const trackColor = Sdata["trackColor"] || "inherit";
+  const thumbColor = Sdata["thumbColor"] || "#333";
+
+  let before = document.head.firstElementChild;
+
+  function addCustomScrollbarStyle() {
+    const scrollStyle = document.createElement("style");
+    scrollStyle.textContent = `   
+  ::-webkit-scrollbar {
+    /* --height: default; */
+    width: ${width};
+    height:${height}; 
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: ${thumbColor};
+    border-radius: ${borderRd};
+    width: 1px;
+  }
+  ::-webkit-scrollbar-track {
+    background-color: ${trackColor};
+  }
+  ::-webkit-scrollbar-corner {
+    background-color: ${trackColor};
+  }
+  
     `;
 
-  document.head.insertBefore(scrollStyle, before);
-}
+    document.head.insertBefore(scrollStyle, before);
+  }
 
-// Add custom scrollbar styles when the page is loaded
-addCustomScrollbarStyle();
+  // Add custom scrollbar styles when the storage data is retrieved
+  addCustomScrollbarStyle();
+});
